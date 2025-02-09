c69711b6779c296add32ecff4ce6441ef8006a2a
Ryan McCartney <33418788+ryanmccartney@users.noreply.github.com>
2025-02-09 17:10:34 +0000

    Revert "DVB Metrics Reporting"

--- src/playbackstrategy/msestrategy.js
+++ src/playbackstrategy/msestrategy.js
@@ -1,4 +1,4 @@
-import { MediaPlayer } from "dashjs"
+import { MediaPlayer } from "dashjs/index_mediaplayerOnly"
 import MediaState from "../models/mediastate"
 import WindowTypes from "../models/windowtypes"
 import DebugTool from "../debugger/debugtool"
@@ -520,10 +520,6 @@ function MSEStrategy(mediaSources, windowType, mediaKind, playbackElement, isUHD
     mediaPlayer = MediaPlayer().create()
     mediaPlayer.updateSettings(dashSettings)
     mediaPlayer.initialize(mediaElement, null, true)
-
-    //Allow cookies to be sent with DVBReporting Requests
-    mediaPlayer.setXHRWithCredentialsForType("DVBReporting", true)
-
     modifySource(playbackTime)
   }
 
